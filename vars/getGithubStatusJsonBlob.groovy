5150a1a551c6b89f155a75e44608c645553753cd
Joshua Mack <joshua.mack@connexta.com>
2018-06-04 21:10:25 -0700

    Updated String used in getGithubStatusJsonBlob

--- vars/getGithubStatusJsonBlob.groovy
+++ vars/getGithubStatusJsonBlob.groovy
@@ -1,3 +1,3 @@
 def call(String state, String target_url, String description, String context) {
-    return "{'state': '${state}', 'target_url': '${target_url}', 'description': '${description}', 'context': '${context}'}"
+    return "\"{'state': '${state}', 'target_url': '${target_url}', 'description': '${description}', 'context': '${context}'}\""
 }
