37bd7bf33e1fd17f407706763de8ba1b47e63619
Tony Morrison <tony.morrison@connexta.com>
2020-01-21 11:16:24 -0700

    Added vars/postReleaseToGithub.groovy

--- vars/postReleaseToGithub.groovy
+++ vars/postReleaseToGithub.groovy
@@ -1,4 +1,18 @@
 def call(String message, String user, String repository, String credentialsToken, String branch, String tag, String artifactPath) {
+	
+    // Provides a library var to create a Github release and push an artifact up to Github
+    //
+    // This will be done by:
+    // . Add the "github.com/connexta/github-utils-shared-library@master" library to the top of the release job
+    // . Call the var passing the following strings:
+    //   .  path to release notes
+    //   .  repo owner
+    //   .  repo name
+    //   .  ssh key to the github repo
+    //   .  branch
+    //   .  tag
+    //   .  full path to the workspace artifact
+
     script {
             sh """
                 release=\$(curl -XPOST -H "Authorization:token ${credentialsToken}" --data "{\\"tag_name\\": \\"${tag}\\", \\"target_commitish\\": \\"${branch}\\", \\"name\\": \\"${tag}\\", \\"body\\": \\"${message}\\", \\"draft\\": false, \\"prerelease\\": false}" https://api.github.com/repos/${user}/${repository}/releases)
